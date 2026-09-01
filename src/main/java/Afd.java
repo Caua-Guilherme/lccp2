17364fd1123c783624108ca2e19d4f1d7801eb4d
caua.costa <cauacosta0810@gmail.com>
2026-09-01 14:50:39 -0300

    sla

---
+++ src/main/java/Afd.java
@@ -0,0 +1,10 @@
+public class Afd {
+
+    public static void main(String[] agrs) {
+
+        String caf = "cafe";
+        System.out.print(caf);
+         caf = "cafezinho";
+        System.out.println(caf);
+    }
+}
